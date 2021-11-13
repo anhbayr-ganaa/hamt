c3cfead65e06334c33f5975f71e4b47378e41ae7
anhbayr-ganaa <g.ankhbayar.st@gmail.com>
2021-11-13 12:21:11 +0800

    first

---
+++ js/myswiper.js
@@ -0,0 +1,25 @@
+
+const swiper = new Swiper('.fade', {
+  effect: 'fade',
+fadeEffect: {
+crossFade: true
+},
+pagination: {
+el: '.swiper-pagination',
+type: 'bullets',
+}, autoplay: {
+delay: 121000,
+},
+
+});
+
+
+
+
+const swiper = new Swiper('.coverswiper', {
+  loop: true,
+ autoplay: {
+   delay: 1000,
+ },
+});
+
